599f2985698e4fe894825cbf1879a99d75cccbb9
jbaxe2 <jbaxe2@gmail.com>
2017-06-22 15:57:23 -0400

    2017-06-22, 15:57

--- lib/cross_listing_view.dart
+++ lib/cross_listing_view.dart
@@ -13,7 +13,7 @@ import 'package:polymer_elements/iron_signals.dart';
 import 'package:polymer_elements/paper_card.dart';
 import 'package:polymer_elements/paper_icon_button.dart';
 
-import 'data_models.dart';
+import 'data_models.dart' show BannerSection, CrossListing, getRequestedSection;
 
 import 'plato_elements_utils.dart';
 
