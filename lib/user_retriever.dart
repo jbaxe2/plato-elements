6948d15dd8eed48672e73a66782cdad2c5409142
jbaxe2 <jbaxe2@gmail.com>
2017-07-06 15:32:41 -0400

    2017-07-06, 15:32

--- lib/user_retriever.dart
+++ lib/user_retriever.dart
@@ -62,10 +62,6 @@ class UserRetriever extends PolymerElement {
 
       this.fire ('iron-signal', detail: {'name': 'hide-progress', 'data': null});
       this.fire ('iron-signal', detail: {'name': 'user-retrieved-complete', 'data': null});
-
-      this.fire ('iron-signal', detail: {
-        'name': 'course-request-submittable', 'data': {'crfSubmittable': true}
-      });
     }
   }
 }
